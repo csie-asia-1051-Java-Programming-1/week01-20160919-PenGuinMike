3b137b8a3b4a41a8dd68840f1bd1670d5224b370
PenGuinMike <104021074@live.asia.edu.tw>
2016-09-19 15:47:00 +0800

    hw04更改

---
+++ week01_20160919/src/hw/hw04_104021074.java
@@ -0,0 +1,15 @@
+package hw;
+/*
+ * Topic: 請寫一個程式讓使用者輸入 a 與 b, 程式輸出 a 開 b 次方後的結果，例如 9.0 開 3次方 結果為 2.080083823051904
+ * Date: 2016/09/19
+ * Author: 104021074 楊永聖
+ */
+import java.util.Scanner;
+public class hw04_104021074 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
