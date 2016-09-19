b6da59acbece57300d4daf11d99fe9dfa88ec501
PenGuinMike <104021074@live.asia.edu.tw>
2016-09-19 15:57:46 +0800

    筆記

--- week01_20160919/src/hw/hw02_104021074.java
+++ week01_20160919/src/hw/hw02_104021074.java
@@ -6,7 +6,12 @@ package hw;
  */
 import java.util.Scanner;
 public class hw02_104021074 {
-
+//筆記 :math.min(a,b)=>取最小,只能比兩個
+	//Random rnd=newRandom() *****import java.util.Random;=>亂數產生
+	//int v1=rnd.nextInt(42)+1 =>亂數產生
+	//math.ceil(無條件進位)(Math.log(17)/log(2))=>log(17)
+	//floor(無條件捨去).round(四捨五入) 
+	//第四題 Math.pow(9.0,1/3.0)九的三分之一次方
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
