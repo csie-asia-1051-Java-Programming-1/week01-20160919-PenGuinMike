7a8671556e7f1b51c5449516c39bf13a73ed658a
PenGuinMike <104021074@live.asia.edu.tw>
2016-09-20 19:08:23 +0800

    hw3存檔

--- week01_20160919/src/hw/hw03_104021074.java
+++ week01_20160919/src/hw/hw03_104021074.java
@@ -4,11 +4,39 @@ package hw;
  * Date: 2016/09/19
  * Author: 104021074 楊永聖
  */
+//筆記 :math.min(a,b)=>取最小,只能比兩個
+	//Random rnd=newRandom() *****import java.util.Random;=>亂數產生
+	//int v1=rnd.nextInt(42)+1 =>亂數產生
+	//math.ceil(無條件進位)(Math.log(17)/log(2))=>log(17)
+	//floor(無條件捨去).round(四捨五入) 
+	//第四題 Math.pow(9.0,1/3.0)九的三分之一次方
 import java.util.Scanner;
 public class hw03_104021074 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn =new Scanner(System.in);
+		System.out.println("請輸入a:");
+		float a= scn.nextFloat();
+		System.out.println("請輸入b:");
+		float b= scn.nextFloat();
+		float x= Math.min(a, b);
+		System.out.println("請輸入c:");
+		float c= scn.nextFloat();
+		float y= Math.min(c, x);
+		System.out.println("請輸入d:");
+		float d= scn.nextFloat();
+		float z= Math.min(d, y);
+		System.out.println("請輸入e:");
+		float e= scn.nextFloat();
+		float w= Math.min(z, e);
+		System.out.println("請輸入f:");
+		float f= scn.nextFloat();
+		float v= Math.min(f, w);
+		float q= Math.round(v);
+		System.out.println("四捨五入後的最小值為:"+q);
+		
+		
 
 	}
 
